3cdfb898f4b4059aa19b8c62a09e6ec4a75f112a
Rowby Villanueva <116034103+rowby7@users.noreply.github.com>
2025-11-24 19:18:05 -0500

    Initial Commit

---
+++ SceneSpotter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SceneSpotter
+//
+//  Created by Rowby Villanueva on 11/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SceneSpotter/SceneSpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneSpotterApp.swift
+//  SceneSpotter
+//
+//  Created by Rowby Villanueva on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SceneSpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
