4b8fde7ce17ae6091ea40283ca45eeee0e9715d2
Rowby Villanueva <116034103+rowby7@users.noreply.github.com>
2025-11-24 20:02:31 -0500

    changed sign up button to navigation link

--- SceneSpotter/ContentView.swift
+++ SceneSpotter/ContentView.swift
@@ -11,31 +11,31 @@ struct ContentView: View {
     @State private var username: String = ""
     @State private var password: String = ""
     var body: some View {
-        VStack {
-            Text("Scene Spotter")
-                .font(.largeTitle)
-                .bold()
-        }
-        VStack (spacing: 20){
-            TextField("username", text: $username)
-                .textFieldStyle(.roundedBorder)
-                
-            TextField("password", text: $password)
-                .textFieldStyle(.roundedBorder)
-            
-            HStack (spacing: 20){
-                Button("Login" ){
-                    
-                }
-                .buttonStyle(.bordered)
-                
-                Button("Sign Up" ){
-                    
-                }
-                .buttonStyle(.bordered)
+        NavigationStack{
+            VStack {
+                Text("Scene Spotter")
+                    .font(.largeTitle)
+                    .bold()
             }
+            VStack (spacing: 20){
+                TextField("username", text: $username)
+                    .textFieldStyle(.roundedBorder)
+                
+                TextField("password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+                
+                HStack (spacing: 20){
+                    Button("Login" ){
+                        
+                    }
+                    .buttonStyle(.bordered)
+                    
+                    NavigationLink("Sign Up", destination: SignUpView())
+                        .buttonStyle(.bordered)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
