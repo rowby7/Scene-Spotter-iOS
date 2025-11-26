ab259e0457572a61a44f01f58a8f9a8604b9b1d0
Rowby Villanueva <116034103+rowby7@users.noreply.github.com>
2025-11-25 22:24:09 -0500

    created map view

---
+++ SceneSpotter/MapView.swift
@@ -0,0 +1,47 @@
+//
+//  MapView.swift
+//  SceneSpotter
+//
+//  Created by Rowby Villanueva on 11/25/25.
+//
+
+import SwiftUI
+import MapKit
+struct MapView: View {
+    @State private var cameraPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 37.5665, longitude: 126.9780),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    @State private var searchText = ""
+
+    var body: some View {
+        NavigationStack {
+            ZStack(alignment: .topTrailing) {
+                Map(position: $cameraPosition)
+                    .ignoresSafeArea()
+                    .searchable(text: $searchText, prompt: "Search places")
+                
+                NavigationLink {
+                    Text("Account & Settings View")
+                } label: {
+                    Image("profile")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                        
+                }
+                .padding() // spacing from top/right edges
+            }
+
+        }
+    }
+}
+
+
+#Preview {
+    MapView()
+}
